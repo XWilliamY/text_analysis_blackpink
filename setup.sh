c912d8757dd5cfa1d8312e3eb45604c075df0a92
William <xwilliamy@gmail.com>
2020-08-10 16:13:59 -0400

    this might be it

--- setup.sh
+++ setup.sh
@@ -1,13 +1,10 @@
 mkdir -p ~/.streamlit/
+echo "[general]
+email = \"email@com\"
+" > ~/.streamlit/credentials.toml
 
-echo “
-[general]\n
-email = \”w.yang@aya.yale.edu\”\n
-“ > ~/.streamlit/credentials.toml
-
-echo “
-[server]\n
-headless = true\n
-enableCORS=false\n
-port = $PORT\n
-“ > ~/.streamlit/config.toml
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
